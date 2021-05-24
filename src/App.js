081f260720ae5d1e6d721156582475501e6a04cc
su-yash-7 <suyashpradhan7@gmail.com>
2021-05-24 23:26:01 +0530

    Adding Play Pause

--- src/App.js
+++ src/App.js
@@ -6,10 +6,11 @@ import data from "./util";
 function App() {
   const [songs,setSong] = useState(data());
   const [currentSong,setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
   return (
     <div className="App">
       <Song currentSong={currentSong}/>
-      <Player/>
+      <Player isPlaying={isPlaying} setIsPlaying={setIsPlaying} currentSong={currentSong}/>
     </div>
   );
 }
--- src/components/Player.js
+++ src/components/Player.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React,{useRef} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faPlay , faAngleLeft,faAngleRight} from '@fortawesome/free-solid-svg-icons'
 
-const Player = () =>{
+const Player = ({currentSong,isPlaying,setIsPlaying}) =>{
+    const audioRef = useRef(null);
+    const playSongHandler = () =>{
+        if(isPlaying){
+            audioRef.current.pause();
+            setIsPlaying(!setIsPlaying);
+        }else{
+            audioRef.current.play();
+            setIsPlaying(!isPlaying);
+
+        }
+    }
     return(
         <div className="player">
             <div className="time-control">
@@ -12,11 +23,12 @@ const Player = () =>{
             </div>
             <div className="play-control">
                 <FontAwesomeIcon className="play-back" size="2x" icon={faAngleLeft}/>
-                <FontAwesomeIcon className="play" size="2x" icon={faPlay}/>
+                <FontAwesomeIcon onClick={playSongHandler} className="play" size="2x" icon={faPlay}/>
                 <FontAwesomeIcon className="play-forward" size="2x" icon={faAngleRight}/>
                 
 
             </div>
+            <audio ref={audioRef} src={currentSong.audio}></audio>
             </div>
     )
 }
