ea19ee535c315ee93584e2a2e544283ef107af96
su-yash-7 <suyashpradhan7@gmail.com>
2021-05-24 16:41:19 +0530

    Initialize and Creating Componrnt

---
+++ src/components/Song.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Song = () =>{
+    return(
+        <div className="song-container">
+            <h1>Picture</h1>
+            <h1>Song</h1>
+            <h1>Artist</h1>
+        </div>
+    )
+}
+export default Song;
