fc621b6c5e905c68c5ba687d16ab8a9b7b35925c
su-yash-7 <suyashpradhan7@gmail.com>
2021-05-25 10:25:31 +0530

    Update Play Pause

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,6 +1,6 @@
 import React,{useRef, useState} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faPlay , faAngleLeft,faAngleRight} from '@fortawesome/free-solid-svg-icons'
+import {faPlay , faAngleLeft,faAngleRight,faPause} from '@fortawesome/free-solid-svg-icons'
 
 const Player = ({currentSong,isPlaying,setIsPlaying}) =>{
     const audioRef = useRef(null);
@@ -42,7 +42,7 @@ const Player = ({currentSong,isPlaying,setIsPlaying}) =>{
             </div>
             <div className="play-control">
                 <FontAwesomeIcon className="play-back" size="2x" icon={faAngleLeft}/>
-                <FontAwesomeIcon onClick={playSongHandler} className="play" size="2x" icon={faPlay}/>
+                <FontAwesomeIcon onClick={playSongHandler} className="play" size="2x" icon={isPlaying ? faPause : faPlay}/>
                 <FontAwesomeIcon className="play-forward" size="2x" icon={faAngleRight}/>
                 
 
